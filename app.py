26d307529a4cc9fa3e62179a8825193016cafcab
mrunal-crest <134675089+mrunal-crest@users.noreply.github.com>
2024-06-04 22:40:18 +0530

    used faiss module of langchain

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
+import os
 import streamlit as st
 from langchain_openai import OpenAIEmbeddings
-import os
 from pinecone import Pinecone
 from openai import OpenAI
 from dotenv import load_dotenv
+from main import generate_chain
+
+# Load environment variables
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_APIKEY = os.getenv('PINECONE_APIKEY')
-import json
-def generate_response(prompt,pdf_url):
+
+def generate_response(prompt, pdf_url):
     client = OpenAI()
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY,model="text-embedding-ada-002")
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY, model="text-embedding-ada-002")
     text_embeddings = embeddings.embed_query(text=prompt)
     pc = Pinecone(api_key=PINECONE_APIKEY)
     index = pc.Index("test")
@@ -30,47 +33,58 @@ def generate_response(prompt,pdf_url):
         raw_text = metadata['raw_text']
         raw_texts += raw_text
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo-16k",
-    messages=[
-        {"role": "system", "content": f"You are a helpful assistant.PLeas generate the proper output from this raw text for user input {raw_texts}"},
-        {"role": "user", "content": prompt}
-    ]
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": f"You are a helpful assistant. Please generate the proper result from this raw text for user input {raw_texts}"},
+            {"role": "user", "content": prompt}
+        ]
     )
     return completion.choices[0].message.content
-# qa1 = generate_model()
-st.title("Langchain Chat Bot")
 
-pdf_select = st.sidebar.radio("Please select your PDF",["Airbus-Annual-Report-2023","annualreport2223","Sample PDF"],index=0)
+def chat_app():
+    try:
+        # Streamlit app title
+        st.title("Langchain Chat Bot")
 
+        # Sidebar PDF selection
+        pdf_select = st.sidebar.radio("Please select your PDF", ["Airbus-Annual-Report-2023", "annualreport2223", "Sample PDF"], index=0)
+        if pdf_select == "Airbus-Annual-Report-2023":
+            pdf_path = f"{os.getenv('AIRBUS_ANNUAL_REPORT')}"
+        elif pdf_select == "Sample PDF":
+            pdf_path = f"{os.getenv('SAMPLE_PDF')}"
+        else:
+            pdf_path = f"{os.getenv('ANNUAL_REPORT_2223')}"
+        print(pdf_path)
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-chat_history = []
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    if pdf_select == "Airbus-Annual-Report-2023":
-        pdf_url = "https://www.airbus.com/sites/g/files/jlcbta136/files/2024-03/Airbus-Annual-Report-2023.pdf"
-        result = generate_response(prompt,pdf_url)
-    elif pdf_select == "Sample PDF":
-        pdf_url = "https://nett.umich.edu/sites/default/files/docs/pdf_files_scan_create_reducefilesize.pdf"
-        result = generate_response(prompt,pdf_url)
-    else:
-        pdf_url = "https://www.singaporeair.com/saar5/pdf/Investor-Relations/Annual-Report/annualreport2223.pdf"
-        result = generate_response(prompt,pdf_url)
-    response = f"Response: {result}"
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+        # React to user input
+        if prompt := st.chat_input("What is up?"):
+            # Display user message in chat message container
+            st.chat_message("user").markdown(prompt)
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            
+            
+            with st.spinner('Generating response...'):
+                result = generate_chain(pdf_path,prompt)
+                response = f"Response: {result}"
+            
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+        
+    except Exception as e:
+        st.error(f"An error occurred in the chat app: {e}")
+        
+if __name__ == "__main__":
+    chat_app()
--- main.py
+++ main.py
@@ -2,54 +2,83 @@ import streamlit as st
 import uuid
 from PyPDF2 import PdfReader
 from pinecone import Pinecone
-from langchain.vectorstores import Weaviate, FAISS
+import tiktoken
+# from langchain.document_loaders import PyPDFLoader
 from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 import os
+from langchain_community.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS, Chroma
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.llms import OpenAI
+from langchain_openai import OpenAI
 import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_APIKEY = os.getenv('PINECONE_APIKEY')
+def estimate_tokens(text, model="gpt-4"):
+    try:
+        # Initialize the tokenizer
+        encoding = tiktoken.encoding_for_model(model)
+        
+        # Encode the text
+        tokens = encoding.encode(text)
+        
+        # Return the number of tokens
+        return len(tokens)
+    except Exception as e:
+        print(f"An error occurred while estimating tokens: {e}")
+        return None
 
-def generate_vector(pdf_path,pdf_url):
-    pdf_reader = PdfReader(pdf_path)
+def generate_chain(pdf_path,prompt):
+    try:
+        pdf_reader = PdfReader(pdf_path)
 
-    text = ""
-    for i, page in enumerate(pdf_reader.pages):
-        text1 = page.extract_text()
-        if text1:
-            text += text1
+        texts = ""
+        for i, page in enumerate(pdf_reader.pages):
+            text1 = page.extract_text()
+            if text1:
+                texts += text1
+                
+        token_count = estimate_tokens(texts)
+        print(f"The estimated number of tokens in the PDF is: {token_count}")
             
-    print(text,"******",len(text))
-    text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 500,
-    chunk_overlap  = 32,
-    length_function = len,
-    )
-    
-    texts = text_splitter.split_text(text)
-
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY,model="text-embedding-ada-002")
-
-    for i in texts:
-        text_embeddings = embeddings.embed_query(text=i)
-
-    pc = Pinecone(api_key=PINECONE_APIKEY)
-    index = pc.Index("test")
-    vector = {  
-            "id":str(uuid.uuid4()),
-            "values":text_embeddings,
-            "metadata":{
-            "raw_text": text,
-            "pdf_url": pdf_url
-            }
-        }
-
-    index.upsert(vectors=[vector],namespace="ns1")
-    
+        text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap  = 32,
+        length_function = len,
+        )
+        texts = text_splitter.split_text(texts)
+        embeddings = OpenAIEmbeddings()
+        docsearch = FAISS.from_texts(texts, embeddings)
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
+        docs = docsearch.similarity_search(prompt)
+        inputs = {'input_documents': docs, 'question': prompt}
+        result = chain.invoke(input=inputs)
+        return result['output_text']
+    except Exception as e:
+        print(f"An error occurred while generating the chain: {e}")
+        return None
+ 
 # generate_vector("D:/Project/Langchain ChatBot/langchain-multi-doc-pdf-chatbot/annualreport2223.pdf","https://www.singaporeair.com/saar5/pdf/Investor-Relations/Annual-Report/annualreport2223.pdf")
 # generate_vector("D:/Project/Langchain ChatBot/langchain-multi-doc-pdf-chatbot/Airbus-Annual-Report-2023.pdf","https://www.airbus.com/sites/g/files/jlcbta136/files/2024-03/Airbus-Annual-Report-2023.pdf")
-generate_vector("D:/Project/Langchain ChatBot/langchain-multi-doc-pdf-chatbot/pdf_files_scan_create_reducefilesize.pdf","https://nett.umich.edu/sites/default/files/docs/pdf_files_scan_create_reducefilesize.pdf")
+# generate_chain("D:/Project/Langchain ChatBot/langchain-multi-doc-pdf-chatbot/pdf_files_scan_create_reducefilesize.pdf","https://nett.umich.edu/sites/default/files/docs/pdf_files_scan_create_reducefilesize.pdf")
+
+
+    # embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY,model="text-embedding-ada-002")
+    # pc = Pinecone(api_key=PINECONE_APIKEY)
+    # index = pc.Index("test")
+
+    # for i in texts:
+    #     text_embeddings = embeddings.embed_query(text=i)
+    #     print("inserted")
+    #     vector = {  
+    #             "id":str(uuid.uuid4()),
+    #             "values":text_embeddings,
+    #             "metadata":{
+    #             "raw_text": i,
+    #             "pdf_url": pdf_url
+    #             }
+    #         }
+
+    #     index.upsert(vectors=[vector],namespace="ns1")
